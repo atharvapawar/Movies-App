a8c75dc176ba690dbd74b958b68ea6c4734939b2
Atharv <atharvapawar34s@gmail.com>
2025-07-24 14:17:35 +0530

    server initial setup almost done

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,3 @@
+import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
+import asyncHandler from "../middlewares/asyncHandler.js";
---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+//controllers
+//middlewares
+const router = express.Router();
+
+router.route("/").post(createUser);
+
+export default router
